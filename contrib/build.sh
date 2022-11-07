52e954035aa2b75c4709b2cdbfc34a477edb4362
Jonathan Logan <JonathanLogan@users.noreply.github.com>
2022-11-07 05:52:59 +0400

    Update build.sh

--- contrib/build.sh
+++ contrib/build.sh
@@ -2,6 +2,8 @@
 docker build --no-cache -f Dockerfile.database -t nearaurora/srpc-database:latest .
 docker build --no-cache -f Dockerfile.indexer -t nearaurora/srpc-indexer:latest .
 docker build --no-cache -f Dockerfile.endpoint -t nearaurora/srpc-endpoint:latest .
+docker build --no-cache -f Dockerfile.refiner -t nearaurora/srpc-refiner:latest .
 docker push nearaurora/srpc-endpoint:latest
 docker push nearaurora/srpc-database:latest
 docker push nearaurora/srpc-indexer:latest
+docker push nearaurora/srpc-refiner:latest
